c0dd73047b14b3a9fc8142d11aa2ad48389cf9c3
lpmatos <luccapessoamatos@gmail.com>
2020-07-30 00:47:36 -0300

    feat: adding regex validation to number

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -5,11 +5,12 @@ set -o errexit
 
 # Main function
 function main(){
+  if ! [[ $1 =~ ^[0-9]+$ ]]; then echo "We need a number" && exit 1; fi
   local THREE=$(expr $1 % 3) FIVE=$(expr $1 % 5) SEVEN=$(expr $1 % 7) RESULT=""
   [ $THREE -eq 0 ] && RESULT+="Pling"
   [ $FIVE -eq 0 ] && RESULT+="Plang"
   [ $SEVEN -eq 0 ] && RESULT+="Plong"
-  [ -z ${RESULT} ] && echo $1 || echo $RESULT
+  echo ${RESULT:-$1}
 }
 
 main "$@"
